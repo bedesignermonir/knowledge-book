1a3bbad2ded909a48369316e7f5f6acce1aa2b76
bedesignermonir <clkmonir@gmail.com>
2025-07-16 13:21:26 +0600

    Border reduce on Bookmark

--- src/component/bookmark/Bookmarks.jsx
+++ src/component/bookmark/Bookmarks.jsx
@@ -2,7 +2,7 @@
 
 const Bookmarks = () => {
     return (
-        <div className="border-4 md:w-1/3">
+        <div className="border-2 md:w-1/3">
             <h1>This is Bookmark</h1>
         </div>
     );
